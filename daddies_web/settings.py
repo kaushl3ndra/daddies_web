39a5b71cd5b738b664e92b10cadf5199f51c9f0d
kaushl3ndra <singh.kas999@gmail.com>
2020-11-26 16:17:31 +0530

    heroku

--- daddies_web/settings.py
+++ daddies_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
